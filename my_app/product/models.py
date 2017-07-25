2a5fa9dee3da2c5a00de36dd380ca8fd50809e4d
lornatumuhairwe <lornatumuhairwe@gmail.com>
2017-07-25 15:05:32 +0300

    added models for Users, Bucketlists and buckelistItems

--- my_app/product/models.py
+++ my_app/product/models.py
@@ -18,5 +18,38 @@ class User(db.Model):
         self.birth_date = birth_date
 
     def __repr__(self):
-        return '<User %d>' % self.email
+        return '<User %r>' % self.email
 
+class Bucketlist(db.Model):
+    __tablename__ = 'bucketlists'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id')) #has to be similar to the table name that it's coming from
+    items = db.relationship('BucketlistItem', backref='bucketlist', lazy='dynamic')
+
+    def __init__(self, name, owner_id, items):
+        self.name = name
+        self.owner = owner_id
+        self.items = items
+
+    def __repr__(self):
+        return '<Bucketlist %r belongs to %r>' % (self.name, self.owner)
+
+class BucketlistItem(db.Model):
+    __tablename__ = 'BLitems'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    deadline = db.Column(db.String(20))
+    status = db.Column(db.String(20))
+    bucketlist_id = db.Column(db.Integer, db.ForeignKey('bucketlists.id'))
+
+    def __init__(self, title, deadline, status, bucketlist_id):
+        self.title = title
+        self.deadline = deadline
+        self.status = status
+        self.bucketlist = bucketlist_id
+
+    def __repr__(self):
+        return '<Item %r belongs to %r bucketlist>' % (self.title, self.bucketlist)
