ea90495b71c066b25460c6b1eea4bf99b7729448
lornatumuhairwe <lornatumuhairwe@gmail.com>
2017-07-27 18:32:08 +0300

    transfered bucketlist APIs to separate file

--- my_app/product/views.py
+++ my_app/product/views.py
@@ -5,13 +5,11 @@ from my_app.product.models import User, Bucketlist, BucketlistItem
 
 catalog = Blueprint('catalog', __name__)
 authentication = Blueprint('authentication', __name__)
-bucketlist = Blueprint('bucketlist', __name__)
 
 @catalog.route('/')
 @catalog.route('/home')
 def home():
     return "Welcome to the User Home"
-
 
 @authentication.route('/auth/register', methods=['POST'])
 def register():
@@ -127,145 +125,3 @@ def logout():
             'message': 'Token not found, Login to get one'
         }
     return jsonify(res)
-
-@bucketlist.route('/bucketlists/', methods=['POST', 'GET'])
-def add_or_view_bucketlist():
-    if request.method=='POST':
-        name = request.form.get('name')
-        auth_token = request.headers.get('Authorization')
-        if auth_token:
-            resp = User.decode_auth_token(auth_token)
-            if isinstance(resp, int):
-                user = User.query.filter_by(id=resp).first()
-                item = Bucketlist.query.filter(Bucketlist.owner_id==resp).filter_by(name=name).first()
-                #item = Bucketlist.query.filter_by(name=name).first()
-                if not item:
-                    bucketlist = Bucketlist(name=name)
-                    db.session.add(bucketlist)
-                    user.bucketlists.append(bucketlist) # FK relationship
-                    db.session.commit()
-                    res = {
-                        'buckelist': bucketlist.name
-                    }
-                else:
-                    res = {
-                        'message': 'Bucketlist exists'
-                    }
-            else:
-                res = {
-                    'status': 'fail',
-                    'message': 'Invalid token, Login again'
-                }
-        else:
-            res = {
-                'status': 'fail',
-                'message': 'Token not found, Login to get one'
-            }
-        return jsonify(res)
-
-    if request.method == 'GET':
-        # name = request.form.get('name')
-        auth_token = request.headers.get('Authorization')
-        if auth_token:
-            resp = User.decode_auth_token(auth_token)
-            if isinstance(resp, int):
-                res = {
-                    bucketlist.id: bucketlist.name for bucketlist in Bucketlist.query.filter_by(owner_id=resp).all()
-                }
-            else:
-                res = {
-                    'status': 'fail',
-                    'message': 'Invalid token, Login again'
-                }
-        else:
-            res = {
-                'status': 'fail',
-                'message': 'Token not found, Login to get one'
-            }
-        return jsonify(res)
-
-@bucketlist.route('/bucketlists/<int:bucketlistID>', methods=['PUT','DELETE', 'GET'])
-def view_update_delete_bucketlist(bucketlistID):
-    if request.method=='PUT':
-        newname = request.form.get('newname')
-        auth_token = request.headers.get('Authorization')
-        if auth_token:
-            resp = User.decode_auth_token(auth_token)
-            if isinstance(resp, int):
-                bucketlist = Bucketlist.query.filter(Bucketlist.owner_id == resp).filter_by(id=bucketlistID).first()
-                if bucketlist:
-                    bucketlist.name = newname
-                    db.session.commit()
-                    res = {
-                        bucketlist.id: bucketlist.name
-                    }
-                else:
-                    res = {
-                        'message': 'Bucketlist doesnt exist'
-                    }
-            else:
-                res = {
-                    'status': 'fail',
-                    'message': 'Invalid token, Login again'
-                }
-        else:
-            res = {
-                'status': 'fail',
-                'message': 'Token not found, Login to get one'
-            }
-        return jsonify(res)
-
-    if request.method == 'GET':
-        # name = request.form.get('name')
-        auth_token = request.headers.get('Authorization')
-        if auth_token:
-            resp = User.decode_auth_token(auth_token)
-            if isinstance(resp, int):
-                bucketlist = Bucketlist.query.filter(Bucketlist.owner_id == resp).filter_by(id=bucketlistID).first()
-                if bucketlist:
-                    res = {
-                        bucketlist.id: bucketlist.name
-                    }
-                else:
-                    res = {
-                        'message': 'Bucketlist doesnt exist'
-                    }
-            else:
-                res = {
-                    'status': 'fail',
-                    'message': 'Invalid token, Login again'
-                }
-        else:
-            res = {
-                'status': 'fail',
-                'message': 'Token not found, Login to get one'
-            }
-        return jsonify(res)
-
-    if request.method=='DELETE':
-        auth_token = request.headers.get('Authorization')
-        if auth_token:
-            resp = User.decode_auth_token(auth_token)
-            if isinstance(resp, int):
-                bucketlist = Bucketlist.query.filter(Bucketlist.owner_id == resp).filter_by(id=bucketlistID).first()
-                if bucketlist:
-                    db.session.delete(bucketlist)
-                    db.session.commit()
-                    res = {
-                        'message': 'Bucketlist deleted successfully'
-                    }
-                else:
-                    res = {
-                        'message': 'Bucketlist doesnt exist'
-                    }
-            else:
-                res = {
-                    'status': 'fail',
-                    'message': 'Invalid token, Login again'
-                }
-        else:
-            res = {
-                'status': 'fail',
-                'message': 'Token not found, Login to get one'
-            }
-        return jsonify(res)
