3601546b18330827eb30f24a6d79e71d4529670e
lornatumuhairwe <lornatumuhairwe@gmail.com>
2017-07-29 09:30:27 +0300

    pushed config to github

---
+++ config.py
@@ -0,0 +1,18 @@
+
+class DevelopmentConfigs:
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://lorna:enambi@localhost/Tofaanga'
+    SECRET_KEY = 'I love Flask'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+class TestingConfigs:
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://lorna:enambi@localhost/TofaangaTest'
+    SECRET_KEY = 'I love Flask'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+class ProductionConfigs:
+    DEBUG = False
+    #SQLALCHEMY_DATABASE_URI = 'postgresql://lorna:enambi@localhost/TofaangaTest'
+    # SECRET_KEY = 'I love Flask'
+    # SQLALCHEMY_TRACK_MODIFICATIONS = True
