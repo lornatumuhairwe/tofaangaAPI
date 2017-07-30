e10c6956d64a242260e927f6b4ef87c74766a964
lornatumuhairwe <lornatumuhairwe@gmail.com>
2017-07-30 10:33:05 +0300

    setup flasgger to work in application

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config.DevelopmentConfigs')
 db = SQLAlchemy(app)
-
+from flasgger import Swagger
 
 from my_app.product.views import catalog, authentication
 from my_app.product.bucketlist_views import bucketlist
@@ -13,5 +13,5 @@ app.register_blueprint(catalog)
 app.register_blueprint(authentication)
 app.register_blueprint(bucketlist)
 app.register_blueprint(bucketlistitems)
-
+Swagger(app)
 db.create_all()
