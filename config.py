6ad1b1980569e77f08bec488416c54870da8b8f3
lornatumuhairwe <lornatumuhairwe@gmail.com>
2017-08-01 14:39:10 +0300

    changed database URI for development confings too

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 
 class DevelopmentConfigs:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://lorna:enambi@localhost/Tofaanga'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://lorna:enambi@localhost/TofaangaTest'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/TofaangaTest'
     SECRET_KEY = 'I love Flask'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
