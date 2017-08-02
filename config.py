120e478971f79e72e51e6bef8e73dfec77028714
lornatumuhairwe <lornatumuhairwe@gmail.com>
2017-08-02 12:08:08 +0300

    changed database uri in config

--- config.py
+++ config.py
@@ -2,7 +2,8 @@
 class DevelopmentConfigs:
     DEBUG = True
     # SQLALCHEMY_DATABASE_URI = 'postgresql://lorna:enambi@localhost/Tofaanga'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/Tofaanga'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/Tofaanga'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/TofaangaTest'
     SECRET_KEY = 'I love Flask'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
