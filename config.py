f62d5ab0ad3de5d816a615814dad8b1e495aa00e
lornatumuhairwe <lornatumuhairwe@gmail.com>
2017-08-02 11:05:35 +0300

    changed configs and tests to match local repository

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 
 class DevelopmentConfigs:
     DEBUG = True
-    #SQLALCHEMY_DATABASE_URI = 'postgresql://lorna:enambi@localhost/TofaangaTest'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/Tofaanga'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://lorna:enambi@localhost/Tofaanga'
+    #SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/Tofaanga'
     SECRET_KEY = 'I love Flask'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 class TestingConfigs:
     DEBUG = True
-    #SQLALCHEMY_DATABASE_URI = 'postgresql://lorna:enambi@localhost/TofaangaTest'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/TofaangaTest'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://lorna:enambi@localhost/TofaangaTest'
+    #SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/TofaangaTest'
     SECRET_KEY = 'I love Flask'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
