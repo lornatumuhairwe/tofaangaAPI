ec8f47f55540188eae19c3213f1f2d407e5e4b4e
lornatumuhairwe <lornatumuhairwe@gmail.com>
2017-08-07 02:28:42 +0300

    fixed bug in bucklistitem views API documentation

--- my_app/product/bucketlistitems_views.py
+++ my_app/product/bucketlistitems_views.py
@@ -7,6 +7,48 @@ bucketlistitems = Blueprint('bucketlistitems', __name__)
 
 @bucketlistitems.route('/bucketlists/<int:bucketlistID>/items/', methods=['POST'])
 def add_item_to_bucketlist(bucketlistID):
+    """Add item to bucketlist
+                ---
+                tags:
+                - "Bucketlist operations"
+                consumes:
+                    - "multipart/form-data"
+                produces:
+                    - "application/json"
+                parameters:
+                - name: "Authorization"
+                  in: "header"
+                  description: "Token of a logged in user"
+                  required: true
+                  type: "string"
+                - name: bucketlistID
+                  in: "path"
+                  description: "The ID the bucketlist"
+                  required: true
+                  type: "string"
+                - name: title
+                  in: "formData"
+                  description: "The title for the new item"
+                  required: true
+                  type: "string"
+                - name: deadline
+                  in: "formData"
+                  description: "The deadline for the new item"
+                  required: true
+                  type: "string"
+                - name: status
+                  in: "formData"
+                  description: "The status for the new item"
+                  required: true
+                  type: "string"
+                responses:
+                    200:
+                      description: "Bucketlist item added Successfully"
+                    400:
+                      description: "Bucketlist item addition Failed"
+                    401:
+                      description: "Addition Doesn't exist"
+               """
     if request.method == 'POST':
         title = request.form.get('title')
         deadline = request.form.get('deadline')
@@ -42,8 +84,55 @@ def add_item_to_bucketlist(bucketlistID):
         return jsonify(res)
 
 
-@bucketlistitems.route('/bucketlists/<int:bucketlistID>/items/<int:BLitemID>', methods=['PUT','DELETE'])
-def update_or_delete_item_in_bucketlist(bucketlistID, BLitemID):
+@bucketlistitems.route('/bucketlists/<int:bucketlistID>/items/<int:BLitemID>', methods=['PUT'])
+def update_item_in_bucketlist(bucketlistID, BLitemID):
+    """Update item in bucketlist
+                ---
+                tags:
+                - "Bucketlist operations"
+                consumes:
+                    - "multipart/form-data"
+                produces:
+                    - "application/json"
+                parameters:
+                - name: "Authorization"
+                  in: "header"
+                  description: "Token of a logged in user"
+                  required: true
+                  type: "string"
+                - name: bucketlistID
+                  in: "path"
+                  description: "The ID the bucketlist"
+                  required: true
+                  type: "string"
+                - name: BLitemID
+                  in: "path"
+                  description: "The ID the bucketlist"
+                  required: true
+                  type: "string"
+                - name: title
+                  in: "formData"
+                  description: "The title for the new item"
+                  required: true
+                  type: "string"
+                - name: deadline
+                  in: "formData"
+                  description: "The deadline for the new item"
+                  required: true
+                  type: "string"
+                - name: status
+                  in: "formData"
+                  description: "The status for the new item"
+                  required: true
+                  type: "string"
+                responses:
+                    200:
+                      description: "Bucketlist item added Successfully"
+                    400:
+                      description: "Bucketlist item addition Failed"
+                    401:
+                      description: "Addition Doesn't exist"
+        """
     if request.method == 'PUT':
         title = request.form.get('title')
         deadline = request.form.get('deadline')
@@ -81,6 +170,40 @@ def update_or_delete_item_in_bucketlist(bucketlistID, BLitemID):
             }
         return jsonify(res)
 
+@bucketlistitems.route('/bucketlists/<int:bucketlistID>/items/<int:BLitemID>', methods=['DELETE'])
+def delete_item_in_bucketlist(bucketlistID, BLitemID):
+    """Delete item in bucketlist
+                ---
+                tags:
+                - "Bucketlist operations"
+                consumes:
+                    - "multipart/form-data"
+                produces:
+                    - "application/json"
+                parameters:
+                - name: "Authorization"
+                  in: "header"
+                  description: "Token of a logged in user"
+                  required: true
+                  type: "string"
+                - name: bucketlistID
+                  in: "path"
+                  description: "The ID the bucketlist"
+                  required: true
+                  type: "string"
+                - name: BLitemID
+                  in: "path"
+                  description: "The ID the bucketlist"
+                  required: true
+                  type: "string"
+                responses:
+                    200:
+                      description: "Bucketlist item added Successfully"
+                    400:
+                      description: "Bucketlist item addition Failed"
+                    401:
+                      description: "Addition Doesn't exist"
+        """
     if request.method == 'DELETE':
         auth_token = request.headers.get('Authorization')
         if auth_token:
