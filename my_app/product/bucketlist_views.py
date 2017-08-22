7fdd39081e9048b5cbf35925c7cb6b656e8a6073
lornatumuhairwe <lornatumuhairwe@gmail.com>
2017-08-22 23:00:28 +0300

    features for pagination and searching for bucketlist items and handling both search and pagination simultaneously

--- my_app/product/bucketlist_views.py
+++ my_app/product/bucketlist_views.py
@@ -113,7 +113,21 @@ def view_bucketlist():
         auth_token = request.headers.get('Authorization')
         if auth_token:
             resp = User.decode_auth_token(auth_token)
-            if isinstance(resp, int) and limit:
+            if isinstance(resp, int) and limit and search_name:
+                result = Bucketlist.query.filter(Bucketlist.name.ilike('%'+search_name+'%')).\
+                    filter_by(owner_id=resp).paginate(page=1, per_page=int(limit)).items
+                if len(result) > 0:
+                    res = {
+                        bucketlist.id: bucketlist.name for bucketlist in result
+                    }
+                    return jsonify(res), 200
+                else:
+                    res = {
+                        'message': 'No result matches this search'
+                    }
+                    return jsonify(res), 200
+
+            elif isinstance(resp, int) and limit:
                 limit_result = Bucketlist.query.filter_by(owner_id=resp).paginate(page=1, per_page=int(limit)).items
                 if len(limit_result) > 0:
                     res = {
@@ -125,19 +139,9 @@ def view_bucketlist():
                         'message': 'No bucketlists yet.'
                         }
                     return jsonify(res), 200
-            elif isinstance(resp, int) and not search_name:
-                if len(Bucketlist.query.filter_by(owner_id=resp).all()) > 0:
-                    res = {
-                        bucketlist.id: bucketlist.name for bucketlist in Bucketlist.query.filter_by(owner_id=resp).all()
-                    }
-                    return jsonify(res), 200
-                else:
-                    res = {
-                        'message': 'No bucketlists yet.'
-                    }
-                    return jsonify(res), 200
+
             elif isinstance(resp, int) and search_name:
-                search_result = Bucketlist.query.filter(Bucketlist.name.match('%'+search_name+'%')).\
+                search_result = Bucketlist.query.filter(Bucketlist.name.ilike('%'+search_name+'%')).\
                     filter_by(owner_id=resp).all()
                 if search_result:
                     res = {
@@ -149,6 +153,19 @@ def view_bucketlist():
                         'message': 'Bucketlist not found'
                     }
                     return jsonify(res), 200
+
+            elif isinstance(resp, int):
+                if len(Bucketlist.query.filter_by(owner_id=resp).all()) > 0:
+                    res = {
+                        bucketlist.id: bucketlist.name for bucketlist in Bucketlist.query.filter_by(owner_id=resp).all()
+                    }
+                    return jsonify(res), 200
+                else:
+                    res = {
+                        'message': 'No bucketlists yet.'
+                    }
+                    return jsonify(res), 200
+
             else:
                 res = {
                     'status': 'fail',
@@ -264,9 +281,68 @@ def view_one_bucketlist(bucketlistID):
     if request.method == 'GET':
         # name = request.form.get('name')
         auth_token = request.headers.get('Authorization')
+        search_name = request.args.get('q', '')  # http://localhost:5000/bucketlists/1?q=Oceania
+        limit = request.args.get('limit', '')
         if auth_token:
             resp = User.decode_auth_token(auth_token)
-            if isinstance(resp, int):
+            if isinstance(resp, int) and limit and search_name:
+                bucketlist = Bucketlist.query.filter(Bucketlist.owner_id == resp).filter_by(id=bucketlistID).first()
+                if bucketlist:
+                    result =  BucketlistItem.query.filter(BucketlistItem.bucketlist_id == bucketlistID).\
+                        filter(BucketlistItem.title.ilike('%'+search_name+'%')).paginate(page=1, per_page=int(limit)).items
+                    if len(result) > 0:
+                        res = {
+                            bucketlistItem.id: bucketlistItem.title for bucketlistItem in result
+                        }
+                        return jsonify(res), 200
+                    else:
+                        res = {
+                            'message': 'No result matches this search'
+                        }
+                        return jsonify(res), 200
+            elif isinstance(resp, int) and limit:
+                bucketlist = Bucketlist.query.filter(Bucketlist.owner_id == resp).filter_by(id=bucketlistID).first()
+                if bucketlist:
+                    limit_result = BucketlistItem.query.filter(BucketlistItem.bucketlist_id == bucketlistID).\
+                        paginate(page=1, per_page=int(limit)).items
+                    if len(limit_result) > 0:
+                        res = {
+                            bucketlistItem.id: bucketlistItem.title for bucketlistItem in limit_result
+                        }
+                        return jsonify(res), 200
+                    else:
+                        res = {
+                            'message': 'No items in this bucketlist'
+                        }
+                        return jsonify(res), 200
+                else:
+                    res = {
+                        'message': "Bucketlist doesn't exist"
+                    }
+                    return jsonify(res), 200
+            elif isinstance(resp, int) and search_name:
+                # search_result = Bucketlist.query.filter(Bucketlist.name.match('%'+search_name+'%')).\
+                #     filter_by(owner_id=resp).all()
+                bucketlist = Bucketlist.query.filter(Bucketlist.owner_id == resp).filter_by(id=bucketlistID).first()
+                if bucketlist:
+                    search_result = BucketlistItem.query.filter(BucketlistItem.bucketlist_id == bucketlistID).\
+                        filter(BucketlistItem.title.ilike('%'+search_name+'%'))
+                    if search_result:
+                        res = {
+                            bucketlistItem.id: bucketlistItem.title for bucketlistItem in search_result
+                            }
+                        return jsonify(res), 200
+                    else:
+                        res = {
+                            'message': 'Bucketlist not found'
+                        }
+                        return jsonify(res), 200
+                else:
+                    res = {
+                        'message': 'Bucketlist doesnt exist'
+                    }
+                    return jsonify(res), 400
+            elif isinstance(resp, int):
                 bucketlist = Bucketlist.query.filter(Bucketlist.owner_id == resp).filter_by(id=bucketlistID).first()
                 bucketlistItems = BucketlistItem.query.filter(BucketlistItem.bucketlist_id == bucketlistID).all()
                 if bucketlist:
@@ -279,6 +355,7 @@ def view_one_bucketlist(bucketlistID):
                         res = {
                             'message': 'No items in this bucketlist'
                         }
+                        return jsonify(res), 200
                 else:
                     res = {
                         'message': 'Bucketlist doesnt exist'
