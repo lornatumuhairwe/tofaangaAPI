925419105d2a10aab00a3d5d942be517bfa17d43
lornatumuhairwe <lornatumuhairwe@gmail.com>
2017-08-26 06:07:00 +0300

    Added tearDown method

--- my_app/tests/test_base.py
+++ my_app/tests/test_base.py
@@ -1,13 +1,17 @@
 import unittest
-# import json, flask
 from my_app import db, app, bcrypt
 from my_app.product.models import User, Bucketlist
 
+
 class TestBase(unittest.TestCase):
+
+    """
+    Before each test is run, the database tables are created ,a test user is added with one bucketlist.
+    Database Tables are dropped after the test has been run.
+    """
+
     def setUp(self):
         app.config.from_object('config.TestingConfigs')
-        db.session.close()
-        db.drop_all()
         db.create_all()
         user = User('testi@mail.com', bcrypt.generate_password_hash('testpass').decode('utf-8'))
         db.session.add(user)
@@ -21,3 +25,7 @@ class TestBase(unittest.TestCase):
         self.app = app.test_client()
         self.user = user
         return self.app, self.auth_token, self.user, self.bucketlistID
+
+    def tearDown(self):
+        db.session.close()
+        db.drop_all()
